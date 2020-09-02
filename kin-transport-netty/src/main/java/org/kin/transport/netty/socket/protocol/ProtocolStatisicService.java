157aa25606b106b61bf8f2af528c3e336cc02a60
huangjianqin <18814127639@163.com>
2020-09-02 17:41:07 +0800

    代码优化

--- kin-transport-netty/src/main/java/org/kin/transport/netty/socket/protocol/ProtocolStatisicService.java
+++ kin-transport-netty/src/main/java/org/kin/transport/netty/socket/protocol/ProtocolStatisicService.java
@@ -3,8 +3,9 @@ package org.kin.transport.netty.socket.protocol;
 import org.kin.framework.Closeable;
 import org.kin.framework.JvmCloseCleaner;
 import org.kin.framework.concurrent.ExecutionContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.kin.framework.counter.Counters;
+import org.kin.framework.counter.Reporters;
+import org.kin.framework.log.LoggerOprs;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,36 +17,38 @@ import java.util.concurrent.TimeUnit;
  * @author huangjianqin
  * @date 2019/6/3
  */
-public class ProtocolStatisicService implements Closeable {
-    private static final Logger reqStatisticLog = LoggerFactory.getLogger("reqStatistic");
-    private static final Logger respStatisticLog = LoggerFactory.getLogger("respStatistic");
+public class ProtocolStatisicService implements Closeable, LoggerOprs {
     private static final ProtocolStatisicService INSTANCE = new ProtocolStatisicService();
+    /** 请求数统计 */
+    private static final String REQ_COUNTER = "reqCounter";
+    /** 响应数统计 */
+    private static final String RESP_COUNTER = "respCounter";
+    /** 请求字节数统计 */
+    private static final String REQ_BYTES_COUNTER = "reqBytesCounter";
+    /** 响应字节数统计 */
+    private static final String RESP_BYTES_COUNTER = "respBytesCounter";
 
     static {
         JvmCloseCleaner.DEFAULT().add(INSTANCE);
     }
 
-    /**
-     *
-     */
-    private ProtocolStatisticHolder reqHolder = new ProtocolStatisticHolder();
-    /** */
-    private ProtocolStatisticHolder respHolder = new ProtocolStatisticHolder();
+    public static ProtocolStatisicService instance() {
+        return INSTANCE;
+    }
+
+    //-------------------------------------------------------------------------------------------------------
+
     /** 统计线程 */
-    private ExecutionContext executionContext = ExecutionContext.fix(2, "inoutbound-statisic",
-            1, "inoutbound-statisic-schedule");
+    private final ExecutionContext executionContext = ExecutionContext.fix(2, "statisic",
+            1, "statisic-schedule");
 
     private ProtocolStatisicService() {
         //一分钟打印一次
         executionContext.scheduleAtFixedRate(() -> {
-            logReqStatistic();
-            logRespStatistic();
+            log().info(System.lineSeparator().concat(Reporters.report()));
         }, 1, 1, TimeUnit.MINUTES);
     }
 
-    public static ProtocolStatisicService instance() {
-        return INSTANCE;
-    }
 
     @Override
     public void close() {
@@ -54,63 +57,13 @@ public class ProtocolStatisicService implements Closeable {
 
     //-------------------------------------------------------------------------------------------------------
 
-    private void logReqStatistic() {
-        ProtocolStatisticHolder origin = reqHolder;
-        reqHolder = new ProtocolStatisticHolder();
-        logReqStatistic0(origin);
-    }
-
-    private void logReqStatistic0(ProtocolStatisticHolder origin) {
-        if (origin != null) {
-            if (origin.getRef() > 0) {
-                executionContext.schedule(() -> logReqStatistic0(origin), 50, TimeUnit.MILLISECONDS);
-                return;
-            }
-            logReqStatistic1(origin);
-        }
-    }
-
-    private void logReqStatistic1(ProtocolStatisticHolder origin) {
-        String content = origin.logContent();
-        reqStatisticLog.info(content);
-    }
-
-    //-------------------------------------------------------------------------------------------------------
-
-    private void logRespStatistic() {
-        ProtocolStatisticHolder origin = respHolder;
-        respHolder = new ProtocolStatisticHolder();
-        logRespStatistic0(origin);
-    }
-
-    private void logRespStatistic0(ProtocolStatisticHolder origin) {
-        if (origin != null) {
-            if (origin.getRef() > 0) {
-                executionContext.schedule(() -> logRespStatistic0(origin), 50, TimeUnit.MILLISECONDS);
-                return;
-            }
-            logRespStatistic1(origin);
-        }
-    }
-
-    private void logRespStatistic1(ProtocolStatisticHolder origin) {
-        String content = origin.logContent();
-        respStatisticLog.info(content);
-    }
-
-    //-------------------------------------------------------------------------------------------------------
-
     public void statisticReq(String uuid, long size) {
-        reqHolder.reference();
-        ProtocolStatistic statistic = reqHolder.getstatistic(uuid);
-        statistic.incr(size);
-        reqHolder.release();
+        Counters.increment(REQ_COUNTER, uuid);
+        Counters.increment(REQ_BYTES_COUNTER, uuid, size);
     }
 
     public void statisticResp(String uuid, long size) {
-        respHolder.reference();
-        ProtocolStatistic statistic = respHolder.getstatistic(uuid);
-        statistic.incr(size);
-        respHolder.release();
+        Counters.increment(RESP_COUNTER, uuid);
+        Counters.increment(RESP_BYTES_COUNTER, uuid, size);
     }
 }
