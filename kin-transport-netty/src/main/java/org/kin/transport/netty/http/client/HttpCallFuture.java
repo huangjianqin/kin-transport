bede5deee8657f229bffb7b599d82523ab212b69
huangjianqin <18814127639@163.com>
2021-10-18 00:11:24 +0800

    HttpCallFuture逻辑优化

--- kin-transport-netty/src/main/java/org/kin/transport/netty/http/client/HttpCallFuture.java
+++ kin-transport-netty/src/main/java/org/kin/transport/netty/http/client/HttpCallFuture.java
@@ -1,9 +1,8 @@
 package org.kin.transport.netty.http.client;
 
 
-import org.kin.framework.concurrent.OneLock;
-
 import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -18,10 +17,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class HttpCallFuture implements Future<HttpResponse> {
     /** http call */
     private final HttpCall httpCall;
-    /** 一次性锁 */
-    private final OneLock sync;
+    private final CountDownLatch latch = new CountDownLatch(1);
     /** http response */
     private volatile HttpResponse httpResponse;
+    /** future完成标识 */
+    private volatile boolean finished;
     /** future cancelled 标识 */
     private final AtomicBoolean cancelled = new AtomicBoolean();
     /** 该future绑定的http 传输链接 */
@@ -30,14 +30,14 @@ public class HttpCallFuture implements Future<HttpResponse> {
     public HttpCallFuture(HttpCall httpCall, HttpClient httpClient) {
         this.httpCall = httpCall;
         this.httpClient = httpClient;
-        this.sync = new OneLock();
     }
 
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
         if (!isDone() && cancelled.compareAndSet(false, true)) {
+            done0();
             //仅仅释放锁
-            sync.release(1);
+            latch.countDown();
             return true;
         }
 
@@ -51,13 +51,13 @@ public class HttpCallFuture implements Future<HttpResponse> {
 
     @Override
     public boolean isDone() {
-        return sync.isDone() || isCancelled();
+        return finished;
     }
 
     @Override
-    public HttpResponse get() {
+    public HttpResponse get() throws InterruptedException {
         //获取锁
-        sync.acquire(-1);
+        latch.await();
         if (isDone()) {
             return httpResponse;
         }
@@ -66,7 +66,7 @@ public class HttpCallFuture implements Future<HttpResponse> {
 
     @Override
     public HttpResponse get(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
-        boolean success = sync.tryAcquireNanos(-1, unit.toNanos(timeout));
+        boolean success = latch.await(timeout, unit);
         if (success) {
             if (isDone()) {
                 return httpResponse;
@@ -80,13 +80,18 @@ public class HttpCallFuture implements Future<HttpResponse> {
         if (isDone()) {
             return;
         }
-        httpClient.free();
+        done0();
         //空即retry
         this.httpResponse = httpResponse;
         if (Objects.nonNull(this.httpResponse)) {
             this.httpResponse.setHttpRequest(httpCall.getRequest());
         }
         //释放锁
-        sync.release(1);
+        latch.countDown();
+    }
+
+    private void done0() {
+        finished = true;
+        httpClient.free();
     }
 }
