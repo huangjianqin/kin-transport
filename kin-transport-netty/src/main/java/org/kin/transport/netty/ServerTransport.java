f05d7eba5f0a5562ea86048b5fb6144697b45382
huangjianqin <459564034@qq.com>
2022-11-12 20:45:06 +0800

    移除无用参数

--- kin-transport-netty/src/main/java/org/kin/transport/netty/ServerTransport.java
+++ kin-transport-netty/src/main/java/org/kin/transport/netty/ServerTransport.java
@@ -1,7 +1,5 @@
 package org.kin.transport.netty;
 
-import com.google.common.base.Preconditions;
-
 /**
  * 服务端传输层通用配置
  *
@@ -9,22 +7,4 @@ import com.google.common.base.Preconditions;
  * @date 2022/11/10
  */
 public abstract class ServerTransport extends Transport {
-    /** 绑定端口 */
-    private int port;
-
-    @Override
-    protected void checkRequire() {
-        super.checkRequire();
-        Preconditions.checkArgument(port > 0, "server port must be greater than 0");
-    }
-
-    //setter && getter
-    public int getPort() {
-        return port;
-    }
-
-    public ServerTransport port(int port) {
-        this.port = port;
-        return this;
-    }
 }
